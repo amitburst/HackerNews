37d548327a46de271adfe4b9a5511390416ad42c
Amit Burstein <amit.burstein@gmail.com>
2014-06-03 09:10:01 -0700

    Initial commit

---
+++ HackerNews/MainTableViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MainTableViewController.swift
+//  HackerNews
+//
+//  Created by Amit Burstein on 6/2/14.
+//  Copyright (c) 2014 Amit Burstein. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let storyCellIdentifier = "StoryCell"
+    let hackerNewsApiUrl = "http://hn.amitburst.me/news"
+    
+    var stories = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchStories()
+    }
+    
+    func fetchStories() {
+        let url = NSURL.URLWithString(hackerNewsApiUrl)
+        let request = NSURLRequest(URL: url)
+        let queue = NSOperationQueue()
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: { response, data, error in
+            if error {
+                println(error)
+            } else {
+                let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                 self.stories = json.objectForKey("posts") as NSArray
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
+            }
+        })
+    }
+    
+    override func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.stories.count
+    }
+    
+    override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        let cell = tableView.dequeueReusableCellWithIdentifier(storyCellIdentifier) as UITableViewCell
+        
+        let story : NSDictionary = self.stories[indexPath.row] as NSDictionary
+        let title = story["title"] as String
+        let points = story["points"] as Int
+        let user = (story["user"] as NSDictionary)["username"] as String
+        
+        cell.textLabel.text = title
+        cell.detailTextLabel.text = "\(points) points by \(user)"
+        
+        return cell
+    }
+    
+    
+}
