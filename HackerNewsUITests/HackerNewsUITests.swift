39f3f67228dd992f9f50488b13686f4ee1453597
Amit Burstein <amit.burstein@gmail.com>
2015-07-31 20:31:59 -0700

    Add simple UI test

---
+++ HackerNewsUITests/HackerNewsUITests.swift
@@ -0,0 +1,27 @@
+//
+//  HackerNewsUITests.swift
+//  HackerNewsUITests
+//
+//  Copyright (c) 2015 Amit Burstein. All rights reserved.
+//  See LICENSE for licensing information.
+//
+
+import XCTest
+@testable import HackerNews
+
+class HackerNewsUITests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    continueAfterFailure = false
+    XCUIApplication().launch()
+  }
+  
+  func testUIElementsVisible() {
+    let app = XCUIApplication()
+    XCTAssertEqual(app.navigationBars.element.identifier, "Hacker News")
+    XCTAssertEqual(app.segmentedControls.buttons.elementBoundByIndex(0).label, "Top")
+    XCTAssertEqual(app.segmentedControls.buttons.elementBoundByIndex(1).label, "Newest")
+    XCTAssertEqual(app.segmentedControls.buttons.elementBoundByIndex(2).label, "Show HN")
+  }
+}
--- Podfile
+++ Podfile
@@ -1,1 +1,4 @@
+link_with 'HackerNews', 'HackerNewsTests'
+link_with 'HackerNews', 'HackerNewsUITests'
+
 pod 'Firebase', '2.3.3'
