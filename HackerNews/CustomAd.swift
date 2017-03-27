7d569fb1b5e36368494ae78ea5eb12c6ea8bdba3
Iain Munro <iain17@me.com>
2017-03-27 14:30:21 +0200

    Updated pocket lib to v1.1.0b

--- HackerNews/CustomAd.swift
+++ HackerNews/CustomAd.swift
@@ -12,19 +12,21 @@ import PocketMediaNativeAds
 /**
  Standard AdUnit for TableView
  **/
-open class CustomAd: AbstractAdUnitTableViewCell {
+open class CustomAd: UITableViewCell, NativeViewCell {
   
     @IBOutlet weak var adDescription: UILabel!
     @IBOutlet weak var adName: UILabel!
     @IBOutlet weak var adImage: UIImageView!
     @IBOutlet weak var callToAction: UIButton!
-    
-    open override func render(_ nativeAd: NativeAd) {
-        super.render(nativeAd)
-        self.adName?.text = nativeAd.campaignName
-        self.adDescription?.text = nativeAd.campaignDescription
-        self.adImage.nativeSetImageFromURL(nativeAd.campaignImage)
-        self.callToAction.setTitle(nativeAd.callToActionText, for: UIControlState.normal)
-    }
+  
+  /**
+   Called to define what ad should be shown. Ad unit should call completion handler once static data like images or videos are downloaded. This is so that the automatic dimensions are recalculated.
+   */
+  public func render(_ nativeAd: NativeAd, completion handler: @escaping ((Bool) -> Void)) {
+    self.adName?.text = nativeAd.campaignName
+    self.adDescription?.text = nativeAd.campaignDescription
+    self.adImage.nativeSetImageFromURL(nativeAd.campaignImage, completion: handler)
+    self.callToAction.setTitle(nativeAd.callToActionText, for: UIControlState.normal)
+  }
   
 }
--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@ platform :ios, '9.0'
 
 def pods
   pod 'Firebase', '2.5.0'
-  pod 'PocketMediaNativeAds', :git => 'https://github.com/Pocketbrain/nativeadslib-ios.git', :branch => 'feature/swift-3.0'
+  pod 'PocketMediaNativeAds', :git => 'https://github.com/Pocketbrain/nativeadslib-ios.git', :branch => 'feature/mopub'
 end
 
 target 'HackerNews' do
